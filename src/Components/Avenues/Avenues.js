35a9628e05f22064da3dbd1dcf9311420161028c
KushanS20 <kushanca20@gmail.com>
2024-06-26 03:23:37 +0530

    Avenues Added

--- src/Components/Avenues/Avenues.js
+++ src/Components/Avenues/Avenues.js
@@ -1,9 +1,231 @@
 export default function Avenues(props) {
-
-
-    return (
-        <div className="blogs">
-            <h1 className="text-3xl font-bold text-gray-900">Avenues</h1>
+  return (
+    <div className="blogs ">
+      <div className="mx-5 h-96 sticky top-10 ">
+        <div className=" bg-slate-800 w-1/2 h-96 float-left  rounded-l-xl flex items-center justify-center ">
+          <h1 className="font-medium text-5xl text-white ">
+            International Service
+          </h1>
         </div>
-    )
+        <div className="border-y border-r border-solid bg-white border-black w-1/2 h-96 float-left rounded-r-xl">
+          <div className="m-5">
+            <h1 className="font-semibold text-4xl">
+              International Service Avenue
+            </h1>
+            <p className="mt-5 text-gray-700">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <div className="mt-10 ">
+              <button
+                type="button"
+                class="inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal  text-white bg-[#DB5079] shadow-danger-3 transition duration-150 ease-in-out hover:bg-danger-accent-300 hover:shadow-danger-2 focus:bg-danger-accent-300 focus:shadow-danger-2 focus:outline-none focus:ring-0 active:bg-danger-600 active:shadow-danger-2 motion-reduce:transition-none dark:shadow-black dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+              >
+                Projects
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mx-5 h-96 mt-20 sticky top-10">
+        <div className="border-y border-l border-solid bg-white border-black w-1/2 h-96 float-left rounded-l-xl">
+          <div className="m-5">
+            <h1 className="font-semibold text-4xl">
+              Environmental Service Avenue
+            </h1>
+            <p className="mt-5 text-gray-700">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <div className="mt-10 ">
+              <button
+                type="button"
+                class="inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal  text-white bg-[#DB5079] shadow-danger-3 transition duration-150 ease-in-out hover:bg-danger-accent-300 hover:shadow-danger-2 focus:bg-danger-accent-300 focus:shadow-danger-2 focus:outline-none focus:ring-0 active:bg-danger-600 active:shadow-danger-2 motion-reduce:transition-none dark:shadow-black dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+              >
+                Projects
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className=" bg-slate-800 w-1/2 h-96 float-left  rounded-r-xl flex items-center justify-center ">
+          <h1 className="font-medium text-5xl  text-white">
+            Environmental Service
+          </h1>
+        </div>
+      </div>
+      <div className="mx-5 h-96 mt-20 sticky top-10">
+        <div className=" bg-slate-800 w-1/2 h-96 float-left  rounded-l-xl flex items-center justify-center ">
+          <h1 className="font-medium text-5xl  text-white">
+            Community Service
+          </h1>
+        </div>
+        <div className="border-y border-r border-solid bg-white border-black w-1/2 h-96 float-left rounded-r-xl">
+          <div className="m-5">
+            <h1 className="font-semibold text-4xl">Community Service Avenue</h1>
+            <p className="mt-5 text-gray-700">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <div className="mt-10 ">
+              <button
+                type="button"
+                class="inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal  text-white bg-[#DB5079] shadow-danger-3 transition duration-150 ease-in-out hover:bg-danger-accent-300 hover:shadow-danger-2 focus:bg-danger-accent-300 focus:shadow-danger-2 focus:outline-none focus:ring-0 active:bg-danger-600 active:shadow-danger-2 motion-reduce:transition-none dark:shadow-black dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+              >
+                Projects
+              </button>
+            </div>
+          </div>
+        </div>{" "}
+      </div>
+      <div className="mx-5 h-96 mt-20 sticky top-10">
+        <div className="border-y border-l border-solid bg-white border-black w-1/2 h-96 float-left rounded-l-xl">
+          <div className="m-5">
+            <h1 className="font-semibold text-4xl">Club Service Avenue</h1>
+            <p className="mt-5 text-gray-700">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <div className="mt-10 ">
+              <button
+                type="button"
+                class="inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal  text-white bg-[#DB5079] shadow-danger-3 transition duration-150 ease-in-out hover:bg-danger-accent-300 hover:shadow-danger-2 focus:bg-danger-accent-300 focus:shadow-danger-2 focus:outline-none focus:ring-0 active:bg-danger-600 active:shadow-danger-2 motion-reduce:transition-none dark:shadow-black dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+              >
+                Projects
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className=" bg-slate-800 w-1/2 h-96 float-left  rounded-r-xl flex items-center justify-center ">
+          <h1 className="font-medium text-5xl  text-white">Club Service</h1>
+        </div>
+      </div>
+      <div className="mx-5 h-96 mt-20 sticky top-10">
+        <div className=" bg-slate-800 w-1/2 h-96 float-left  rounded-l-xl flex items-center justify-center ">
+          <h1 className="font-medium text-5xl  text-white">Sports Service</h1>
+        </div>
+        <div className="border-y border-r border-solid bg-white border-black w-1/2 h-96 float-left rounded-r-xl">
+          <div className="m-5">
+            <h1 className="font-semibold text-4xl">Sports Service Avenue</h1>
+            <p className="mt-5 text-gray-700">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <div className="mt-10 ">
+              <button
+                type="button"
+                class="inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal  text-white bg-[#DB5079] shadow-danger-3 transition duration-150 ease-in-out hover:bg-danger-accent-300 hover:shadow-danger-2 focus:bg-danger-accent-300 focus:shadow-danger-2 focus:outline-none focus:ring-0 active:bg-danger-600 active:shadow-danger-2 motion-reduce:transition-none dark:shadow-black dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+              >
+                Projects
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mx-5 h-96 mt-20 sticky top-10">
+        <div className="border-y border-l border-solid bg-white border-black w-1/2 h-96 float-left rounded-l-xl">
+          <div className="m-5">
+            <h1 className="font-semibold text-4xl">Public Relations Avenue</h1>
+            <p className="mt-5 text-gray-700">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <div className="mt-10 ">
+              <button
+                type="button"
+                class="inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal  text-white bg-[#DB5079] shadow-danger-3 transition duration-150 ease-in-out hover:bg-danger-accent-300 hover:shadow-danger-2 focus:bg-danger-accent-300 focus:shadow-danger-2 focus:outline-none focus:ring-0 active:bg-danger-600 active:shadow-danger-2 motion-reduce:transition-none dark:shadow-black dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+              >
+                Projects
+              </button>
+            </div>
+          </div>
+        </div>{" "}
+        <div className=" bg-slate-800 w-1/2 h-96 float-left  rounded-r-xl flex items-center justify-center ">
+          <h1 className="font-medium text-5xl  text-white">Public Relations</h1>
+        </div>
+      </div>
+      <div className="mx-5 h-96 mt-20 sticky top-10">
+        <div className=" bg-slate-800 w-1/2 h-96 float-left  rounded-l-xl flex items-center justify-center ">
+          <h1 className="font-medium text-5xl  text-white">
+            Professional Development
+          </h1>
+        </div>
+        <div className="border-y border-r border-solid bg-white border-black w-1/2 h-96 float-left rounded-r-xl">
+          <div className="m-5">
+            <h1 className="font-semibold text-4xl">
+              Professional Development Avenue
+            </h1>
+            <p className="mt-5 text-gray-700">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <div className="mt-10 ">
+              <button
+                type="button"
+                class="inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal  text-white bg-[#DB5079] shadow-danger-3 transition duration-150 ease-in-out hover:bg-danger-accent-300 hover:shadow-danger-2 focus:bg-danger-accent-300 focus:shadow-danger-2 focus:outline-none focus:ring-0 active:bg-danger-600 active:shadow-danger-2 motion-reduce:transition-none dark:shadow-black dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+              >
+                Projects
+              </button>
+            </div>
+          </div>
+        </div>{" "}
+      </div>
+      <div className="h-20 mt-20">f</div>
+    </div>
+  );
 }
