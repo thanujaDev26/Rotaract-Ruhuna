17a34a03f807de09144f4ab7edc5f9d399de0094
Janith Ranasinghe <janithraditha@gmail.com>
2024-07-03 02:20:59 +0530

    Added responsive and change navBar properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    colors:{
-      "base":'#C62F52',
-      "white":'#ecf0f1',
-      "red":'#EA2027',
-      "text-b":'#1e272e',
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        colors: {
+            "pink": '#C62F52',
+            "white": '#ecf0f1',
+            "red": '#EA2027',
+            "text-b": '#1e272e',
+            "gray-50": '#4b5563',
+            "gray-900": '#111827',
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
 }
 
