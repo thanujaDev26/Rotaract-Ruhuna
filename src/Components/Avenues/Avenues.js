1d39b98301343eafe33a4a2382d9c0c0cc76ce9d
KushanS20 <kushanca20@gmail.com>
2024-07-04 01:06:22 +0530

    updated

--- src/Components/Avenues/Avenues.js
+++ src/Components/Avenues/Avenues.js
@@ -50,38 +50,38 @@ export default function Avenues(props) {
 
     const services = [
         {
-            id: 'InternationalService',
+            id: 0,
             imageUrl: InternationalServiceImage,
             avenue: 'International Service Avenue',
             description: 'The International Service Avenue exemplifies global reach, going beyond boundaries to help the international community. Projects in this avenue focus on cultural exchange, facilitating mutual understanding, and engaging in international service projects. By connecting globally, this avenue contributes to positive change on a broader scale.'
         },
         {
-            id: 'EnvironmentalService',
+            id: 1,
             imageUrl: EnvironmentalServiceImage,
             avenue: 'Environmental Service Avenue',
             description: ''
         },
         {
-            id: 'CommunityService',
+            id: 2,
             imageUrl: CommunityServiceImage,
             avenue: 'Community Service Avenue',
             description: 'The Community Service Avenue identifies critical societal issues and customizes projects to meet people’s needs, creating a better and happier society regardless of demographic differences to improve the quality of life for the local community or university campuses, involving all or most club members. The Environmental Service Avenue also operates under the Community Service Avenue, providing the opportunity to engage with nature, aiming to establish sustainable initiatives and preserve sensitive ecosystems. This avenue fosters community growth and environmental awareness within the club and society.'
         },
         {
-            id: 'ClubService',
+            id: 3,
             imageUrl: ClubServiceImage,
             avenue: 'Club Service Avenue',
             description: 'The Club Service Avenue focuses on the logistical tasks that create an outstanding club experience for both current and newly inducted members. This avenue ensures that our clubs have active, engaged members who can provide service to their communities and attract new members. Under this avenue, the Rotaract Club of the University of Ruhuna organizes activities within our own club, fostering fellowship and leadership development. The Sports and Recreation Service Avenue also operates under the Club Service Avenue, promoting physical well-being, fostering camaraderie, and encouraging an active lifestyle among its members through various sports events, fitness challenges, and recreational activities.'
         },
-        {id: 'SportsService', imageUrl: SportsServiceImage, avenue: 'Sports Service Avenue', description: ''},
+        {id: 4, imageUrl: SportsServiceImage, avenue: 'Sports Service Avenue', description: ''},
         {
-            id: 'PublicRelations',
+            id: 5,
             imageUrl: PublicRelationsImage,
             avenue: 'Public Relations Avenue',
             description: 'The Public Relations Avenue of the Rotaract Club of the University of Ruhuna is instrumental in shaping the club\'s image and building connections. This avenue raises awareness, promotes initiatives, and collaborates effectively within members of the RACRUHUNA. We leverage top-notch strategies across platforms like WhatsApp, Facebook, Instagram, YouTube, LinkedIn, and TikTok to engage the public and maintain a positive image for the club.'
         },
         {
-            id: 'ProfessionalDevelopment',
+            id: 6,
             imageUrl: ProfessionalDevelopmentImage,
             avenue: 'Professional Development Avenue',
             description: 'The Professional Development Avenue empowers undergraduate communities to discover and pursue their dream careers, transforming them into qualified professionals with essential skills and competencies. This focus extends beyond individual clubs to include other undergraduate communities in Sri Lanka and beyond.'
@@ -92,6 +92,7 @@ export default function Avenues(props) {
 
     const showContent = (contentId) => {
         setActiveContent(contentId);
+        console.log(activeContent);
     };
 
     const handleBack = () => {
@@ -169,7 +170,7 @@ export default function Avenues(props) {
                             className="mx-5 sm:mx-10 md:mx-5 lg:mx-5 mb-10 flex flex-col md:flex-row section"
                             ref={(el) => (sectionsRef.current[index] = el)}
                         >
-                            {index % 2 === 0 ? (
+                            {service.id % 2 === 0 ? (
                                 <>
                                     <div
                                         className="bg-slate-800 w-full md:w-1/2 h-96 flex items-center justify-center rounded-t-xl md:rounded-l-xl md:rounded-tr-none">
@@ -183,12 +184,7 @@ export default function Avenues(props) {
                                             {service.description}
                                         </p>
                                         <div className="mt-10">
-                                            <button
-                                                onClick={() => showContent(service.id)}
-                                                className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-                                            >
-                                                Projects
-                                            </button>
+
                                             <button onClick={handleBack}
                                                     className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                                             >
@@ -206,12 +202,7 @@ export default function Avenues(props) {
                                             {service.description}
                                         </p>
                                         <div className="mt-10">
-                                            <button
-                                                onClick={() => showContent(service.id)}
-                                                className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-                                            >
-                                                Projects
-                                            </button>
+
                                             <button onClick={handleBack}
                                                     className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                                             >
@@ -229,7 +220,6 @@ export default function Avenues(props) {
                         </div>
                     ))
             )}
-            <div className="h-20 mt-20">rgrt</div>
         </div>
     );
 }
