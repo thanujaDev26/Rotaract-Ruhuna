20631949fd0589754f7a9952db9e70ea52f145fc
KushanS20 <kushanca20@gmail.com>
2024-10-02 14:26:07 +0530

    Home page and Avenue page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     ],
     theme: {
         extend:{
+            screens: {
+                'below-1530': {'max': '1530px'},
+            },
             colors: {
                 "pink": '#C62F52',
                 "white-1": '#ecf0f1',
@@ -14,10 +17,37 @@ module.exports = {
                 "gray-900": '#111827',
                 "transparent": 'transparent',
             },
+          scale: {
+              'custom-45': '0.45',
+            'custom-75': '0.75',
+            'custom-100': '1',
+          },
+
         }
     },
     plugins: [
-        require('@tailwindcss/forms')
+        require('@tailwindcss/forms'),
+      function ({ addUtilities }) {
+        const newUtilities = {
+          '@media (min-width: 1024px) and (max-width: 1600px)': {
+            '.scale-dynamic': {
+              transform: 'scale(calc(0.75 + 0.25 * ((100vw - 1024px) / 576)))',
+            },
+          },
+          '@media (max-width: 1023px)': {
+            '.scale-50': {
+              transform: 'scale(0.5)',
+            },
+          },
+          '@media (min-width: 1601px)': {
+            '.scale-100': {
+              transform: 'scale(1)',
+            },
+          },
+        };
+
+        addUtilities(newUtilities, ['responsive']);
+      },
     ],
 }
 
